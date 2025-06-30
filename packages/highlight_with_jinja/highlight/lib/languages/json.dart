4361aa6b46a60a61fcd1a0d98affe658531bea3a
Avo <avomandjian@gmail.com>
2025-06-30 13:10:51 +0400

    added packages

---
+++ packages/highlight_with_jinja/highlight/lib/languages/json.dart
@@ -0,0 +1,76 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import '../src/common_modes.dart';
+import '../src/mode.dart';
+
+final json = Mode(
+  refs: {
+    '~contains~2~contains~1~contains~3': Mode(
+      begin: "\\[",
+      end: "\\]",
+      contains: [
+        Mode(
+          end: ",",
+          endsWithParent: true,
+          excludeEnd: true,
+          contains: [
+            QUOTE_STRING_MODE,
+            C_NUMBER_MODE,
+            Mode(ref: '~contains~2'),
+            Mode(ref: '~contains~2~contains~1~contains~3'),
+            C_LINE_COMMENT_MODE,
+            C_BLOCK_COMMENT_MODE,
+          ],
+          keywords: {"literal": "true false null"},
+        ),
+      ],
+      illegal: "\\S",
+    ),
+    '~contains~2': Mode(
+      begin: "{",
+      end: "}",
+      contains: [
+        Mode(className: "attr", begin: "\"", end: "\"", contains: [BACKSLASH_ESCAPE], illegal: "\\n"),
+        Mode(
+          end: ",",
+          endsWithParent: true,
+          excludeEnd: true,
+          contains: [
+            QUOTE_STRING_MODE,
+            C_NUMBER_MODE,
+            Mode(ref: '~contains~2'),
+            Mode(ref: '~contains~2~contains~1~contains~3'),
+            C_LINE_COMMENT_MODE,
+            C_BLOCK_COMMENT_MODE,
+          ],
+          keywords: {"literal": "true false null"},
+          begin: ":",
+        ),
+        C_LINE_COMMENT_MODE,
+        C_BLOCK_COMMENT_MODE,
+      ],
+      illegal: "\\S",
+    ),
+    '~jinja~string': Mode(
+      className: 'string',
+      begin: '"',
+      end: '"',
+      contains: [
+        BACKSLASH_ESCAPE,
+        Mode(ref: '~jinja~var'),
+        Mode(ref: '~jinja~tag'),
+      ],
+    ),
+    '~contains~0': Mode(ref: '~jinja~string'),
+  },
+  contains: [
+    QUOTE_STRING_MODE,
+    C_NUMBER_MODE,
+    Mode(ref: '~contains~2'),
+    Mode(ref: '~contains~2~contains~1~contains~3'),
+    C_LINE_COMMENT_MODE,
+    C_BLOCK_COMMENT_MODE,
+  ],
+  keywords: {"literal": "true false null"},
+  illegal: "\\S",
+);
