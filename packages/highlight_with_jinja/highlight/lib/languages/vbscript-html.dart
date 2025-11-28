45f9ec7a8ab3183a2050075a76de34627c5661b1
Avo <avomandjian@gmail.com>
2025-11-28 14:31:32 +0400

    changed to gitlab pub package

--- packages/highlight_with_jinja/highlight/lib/languages/vbscript-html.dart
+++
@@ -1,9 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-import '../src/mode.dart';
-
-final vbscriptHtml = Mode(refs: {}, subLanguage: [
-  "xml"
-], contains: [
-  Mode(begin: "<%", end: "%>", subLanguage: ["vbscript"])
-]);
